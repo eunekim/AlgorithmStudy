4ba2c71d5bcdddb35ea0d52a8c8a2f920db1561c
402-11 <402-11@DESKTOP-T47BO81>
2025-02-21 17:54:45 +0900

    8393번 문제

--- src/step3/Main_10950.java
+++ src/step3/Main_10950.java
@@ -10,13 +10,14 @@ public class Main_10950 {
 		Scanner scan = new Scanner(System.in);
 		
 		int a = scan.nextInt();
-		int[] nlist = new int[5];
+		int[] nlist = new int[a];
 		
 		for(int i=0; i<a; i++) {
 			int n1 = scan.nextInt();
 			int n2 = scan.nextInt();
 			nlist[i] = n1 + n2;
 		}
+		scan.close();
 		
 		for(int i=0; i<a; i++) {
 			System.out.println(nlist[i]);
---
+++ src/step3/Main_8393.java
@@ -0,0 +1,14 @@
+package step3;
+
+import java.util.Scanner;
+
+public class Main_8393 {
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		
+	}
+
+}
